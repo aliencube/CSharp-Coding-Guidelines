f7e79d3fcbb9c6e25df32c14c6e4392e3f49aab8
Justin Yoo <justin.yoo@aliencube.com>
2014-06-05 15:28:07 +1000

    Temporary saved.

--- js/main.js
+++ js/main.js
@@ -1,39 +1,46 @@
 ; "use strict";
 
-var pages = [
-    { "page": "index", "markdown": "README.md" },
-    { "page": "class-design-guidelines", "markdown": "Class.Design.Guidelines.md" },
-    { "page": "member-design-guidelines", "markdown": "Member.Design.Guidelines.md" },
-    { "page": "miscellaneous-design-guidelines", "markdown": "Miscellaneous.Design.Guidelines.md" },
-    { "page": "maintainability-guidelines", "markdown": "Maintainability.Guidelines.md" },
-    { "page": "naming-guidelines", "markdown": "Naming.Guidelines.md" },
-    { "page": "performance-guidelines", "markdown": "Performance.Guidelines.md" },
-    { "page": "framework-guidelines", "markdown": "Framework.Guidelines.md" },
-    { "page": "documentation-guidelines", "markdown": "Documentation.Guidelines.md" },
-    { "page": "layout-guidelines", "markdown": "Layout.Guidelines.md" }
-];
-
-
 (function ($) {
     $(document).ready(function () {
+        var file = getCurrentPage();
         var lang = getLanguage();
-        var page = pages[0].markdown;
-        getMarkdown(page, lang);
+        $.each(pages, function (i, page) {
+            if (page.page + ".html" == file) {
+                var doc = page.doc;
+                getMarkdown(doc, lang);
+            }
+
+            if (page.page == "index") {
+                return;
+            }
+
+            var li = $("<li></li>").append($("<a></a>").attr("href", page.page + ".html").text(page.name));
+            $("#nav-menu").append(li);
+        });
     });
+
+    // Gets the current page.
+    var getCurrentPage = function() {
+        var page = $.url().attr("file");
+        if (page == undefined || !page.length) {
+            page = "index.html";
+        }
+        return page;
+    };
 
     // Gets the language from the query string.
     var getLanguage = function() {
         var lang = $.url().param("lang");
-        if (lang == undefined || lang.length == 0) {
+        if (lang == undefined || !lang.length) {
             lang = "en";
         }
         return lang;
     };
 
     // Gets the given markdown page.
-    var getMarkdown = function (page, lang) {
+    var getMarkdown = function (doc, lang) {
         var localistion = lang != "en" ? "localisation/" + lang + "/" : "";
-        var url = "https://api.github.com/repos/aliencube/CSharp-Coding-Guidelines/contents/" + localistion + page;
+        var url = "https://api.github.com/repos/aliencube/CSharp-Coding-Guidelines/contents/" + localistion + doc;
         $.ajax({
                 type: "GET",
                 url: url,
@@ -59,7 +66,20 @@ var pages = [
                 dataType: "html"
             })
             .done(function(data) {
+                for (var i in pages) {
+                    if (i == 0) {
+                        continue;
+                    }
+                    var doc = pages[i].doc;
+                    var page = pages[i].page;
+                    data.replace(doc, page + ".html");
+                    console.log(doc + "::" + page);
+                    console.log(data);
+                }
                 $("#main-content").html(data);
             });
+    };
+
+    var replaceDocuments = function () {
     };
 })(jQuery);
---
+++ js/variables.js
@@ -0,0 +1,14 @@
+; "use strict";
+
+var pages = [
+    { "page": "index", "name": "Home", "doc": "README.md" },
+    { "page": "class-design-guidelines", "name": "Class Design", "doc": "Class.Design.Guidelines.md" },
+    { "page": "member-design-guidelines", "name": "Member Design", "doc": "Member.Design.Guidelines.md" },
+    { "page": "miscellaneous-design-guidelines", "name": "Miscellaneous Design", "doc": "Miscellaneous.Design.Guidelines.md" },
+    { "page": "maintainability-guidelines", "name": "Maintainability", "doc": "Maintainability.Guidelines.md" },
+    { "page": "naming-guidelines", "name": "Naming", "doc": "Naming.Guidelines.md" },
+    { "page": "performance-guidelines", "name": "Performance", "doc": "Performance.Guidelines.md" },
+    { "page": "framework-guidelines", "name": "Framework", "doc": "Framework.Guidelines.md" },
+    { "page": "documentation-guidelines", "name": "Documentation", "doc": "Documentation.Guidelines.md" },
+    { "page": "layout-guidelines", "name": "Layout", "doc": "Layout.Guidelines.md" }
+];
